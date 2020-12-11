6c5825bcff3183fa07270c85155f53d6589f09f4
Abdullahi <46778885+Jimlah@users.noreply.github.com>
2020-12-12 00:42:34 +0100

    The organization view is completed

--- app/controllers/Dashboard/AdminController.php
+++ app/controllers/Dashboard/AdminController.php
@@ -5,6 +5,9 @@ namespace App\Controllers\Dashboard;
 use App\Core\Http\Response;
 use App\Core\Http\Request;
 use App\Core\Http\Controller;
+use App\Core\Tools\Auth;
+use App\Models\UsersModel;
+use App\Providers\UsersProvider;
 
 class AdminController extends Controller
 {
@@ -16,6 +19,12 @@ class AdminController extends Controller
 
     public function organization(Request $request, Response $response)
     {   
-        $response->view('/dashboard/admin/organization',[]);
+        Auth::user();
+        $org = UsersModel::select()
+                            ->where('access', UsersProvider::ACCESS_ORGANIZATION)
+                            ->fetchAll();
+        return $response->view('/dashboard/admin/organization',[
+            'org' => $org
+        ]);
     }
 }
