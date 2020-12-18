8e4ce06d212e2e3ea2b359b229bb26c037be573f
Abdullahi <46778885+Jimlah@users.noreply.github.com>
2020-12-18 19:58:16 +0100

    The mailing features completed

--- app/controllers/Dashboard/Admin/OrganizationController.php
+++ app/controllers/Dashboard/Admin/OrganizationController.php
@@ -17,18 +17,18 @@ use Swift_SmtpTransport;
 class OrganizationController extends Controller
 {
     public function display(Request $request, Response $response)
-    {   
+    {
         Auth::user();
         $org = UsersModel::select()
-                            ->where('access', UsersProvider::ACCESS_ORGANIZATION)
-                            ->fetchAll();
-        return $response->view('/dashboard/admin/organization',[
+            ->where('access', UsersProvider::ACCESS_ORGANIZATION)
+            ->fetchAll();
+        return $response->view('/dashboard/admin/organization', [
             'org' => $org
         ]);
     }
 
     public function registerOrg(Request $request, Response $response)
-    {   
+    {
         $user = $request->user();
         $username = $request->input('username');
         $firstname = $request->input('firstname');
@@ -40,49 +40,49 @@ class OrganizationController extends Controller
         $status = UsersProvider::STATUS_ACTIVE;
 
 
-        
+
+
 
         InputValidator::init([
-            "uniqueField" => function (InputValidator $validator, string $field, string $message){
-                if($validator->getValue() == ''){
+            "uniqueField" => function (InputValidator $validator, string $field, string $message) {
+                if ($validator->getValue() == '') {
                     return null;
                     // die();
-                    }
+                }
                 if (UsersModel::findby($field, $validator->getValue())) {
-                    
+
                     $validator->attachError($message);
                 }
-                
             }
-            
+
         ]);
-        
-        $username->validate('required')->uniqueField('username','Username has already been registered');
+
+        $username->validate('required')->uniqueField('username', 'Username has already been registered');
         $lastname->validate('required');
         $firstname->validate('required');
-        $email->validate('required')->uniqueField('email','Email has already been registered');
+        $email->validate('required')->uniqueField('email', 'Email has already been registered');
         $password->validate('required')->equals($password1, "Password does not match");
 
-        if(!InputValidator::isValid()){
+        if (!InputValidator::isValid()) {
             $errors = InputValidator::getErrors();
-            
+
             return $response->withSession('msg', [$errors, 'error'])->redirect($request->url()->getPath());
         }
 
-        $name = $firstname;
-        $message = "Thank you for applying to for this Application.<br>
-                    Username: $username <br>
-                    Email: $email <br> 
-                    Password: $password <br> 
+        $message = "Thank you for applying to for this Application.
+                    Username: $username 
+                    Email: $email 
+                    Password: $password  
                     goto http://fmcdufma.demisho.com.ng/sign_in to login
                     and make sure you change your password immediately";
 
 
-        SendMailController::send(
-            $email, 
-            $name, 
+        $msgerror = $mail = SendMailController::send(
+            $email,
             $message
         );
+
+
 
         UsersModel::createEntry([
             'userid' => $user->id(),
@@ -95,11 +95,13 @@ class OrganizationController extends Controller
             'status' => $status
         ]);
 
-        $msg = 'You have successfully logrd in';
+        $msg = 'You have successfully loggedd in';
+
+        if ($msgerror) {
+
+            $msg = $msgerror;
+        }
 
         return $response->withSession('msg', [$msg, 'alert'])->redirect($request->url()->getPath());
-
     }
-
-    
 }
--- app/controllers/Dashboard/SendMailController.php
+++ app/controllers/Dashboard/SendMailController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controllers\Dashboard;
 
-use App\Core\Http\Response;
-use App\Core\Http\Request;
+
 use App\Core\Http\Controller;
 use Swift_SmtpTransport;
 use Swift_Mailer;
 use Swift_Message;
+use Swift_RfcComplianceException;
 
 class SendMailController extends Controller
 {
@@ -18,21 +18,27 @@ class SendMailController extends Controller
      * @param name: the name of the person
      * @param msg: the message
      */
-    public static function send($email, $name = null, $msg)
-    {   
-        $transport = (new Swift_SmtpTransport('mail.dufma.ng', 25))
-                    ->setUsername('admin@fmcdufma.demisho.com.ng')
-                    ->setPassword('2-Jae1li1bm');
-        
-        $mailer = new Swift_Mailer($transport);
+    public static function send($email, $msg)
+    {
+        try {
+            $transport = (new Swift_SmtpTransport('mail.dufma.ng', 25))
+                ->setUsername('admin@fmcdufma.demisho.com.ng')
+                ->setPassword('2-Jae1li1bm');
+
+            $mailer = new Swift_Mailer($transport);
 
 
-        $message = (new Swift_Message('WonderFul Subject'))
-                    ->setFrom(['admin@fmcdufma.demisho.com.ng' => 'Dufma'])
-                    ->setTo([$email => $name])
-                    ->setBody($msg);
+            $message = (new Swift_Message('WonderFul Subject'))
+                ->setFrom(['admin@fmcdufma.demisho.com.ng' => 'Dufma'])
+                ->setTo("$email")
+                ->setBody($msg);
 
 
-        $result = $mailer->send($message);
+            $mailer->send($message);
+            return;
+        } catch (Swift_RfcComplianceException $e) {
+            $msg = $e->getMessage();
+            return $msg;
+        }
     }
 }
